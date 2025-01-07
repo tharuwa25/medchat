38a6bfbf95bb684a3c0a81f66c1dc70ccb87f1d1
Tharusha Dilshan <tharushadil11@gmail.com>
2025-01-07 20:08:22 +0530

    fix error 10

--- app/(root)/results/page.tsx
+++ app/(root)/results/page.tsx
@@ -100,8 +100,8 @@ const Results = () => {
                     </p>
                 ))}
             </div> */}
-            <ResultYes diseaes={diseaes} disc={disc} prevention={prevention}/>
-                </>
+<ResultYes diseaes={diseaes ?? ''} disc={disc} prevention={prevention} />
+</>
             )}
 
             {/* <button onClick={updateAllergy}>Update Allergy to False</button> */}
