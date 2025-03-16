657e277c4aa094c688fb0980e19b443e44a38108
Tharusha Dilshan <tharushadil11@gmail.com>
2025-03-16 17:01:35 +0530

    deploy fixed 012

--- components/Welcome.tsx
+++ components/Welcome.tsx
@@ -29,7 +29,7 @@ export function Welcome() {
           MEDCHAT
         </div>
         <div className="font-extralight text-base md:text-4xl dark:text-neutral-200 py-4">
-          CHeck your symptom today ! 
+        Your symptoms, analyzed instantly. 
         </div>
         {/* <button className="bg-blue-800 dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2">
           TRY NOW
